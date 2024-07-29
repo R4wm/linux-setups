e59d8312df1108fde67368225cf3052880619647
r4wm <raymondmintz11@gmail.com>
2024-07-28 22:11:08 -0400

    hi Savannah

---
+++ installs/ubuntu/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# a collection of things i use all the time
+
+## install golang
+cd /tmp
+wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
+
+## install ubuntu packages
+apt install -y git subversion jq bat wget
