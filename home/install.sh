d88a01611d575bc5a2331c6cb925a365d293e592
r4wm <raymondmintz11@gmail.com>
2024-08-14 11:49:57 -0400

    emojis

--- home/install.sh
+++ home/install.sh
@@ -2,3 +2,11 @@
 
 cd ~/github
 git clone https://github.com/gcholette/bash-ps1-themes
+
+cd /tmp
+git clone https://github.com/dysfunc/ascii-emoji.git
+cd ascii-emoji/
+mv emojis ~/bin/
+cd ~
+rm -rf /tmp/ascii-emoji
+
